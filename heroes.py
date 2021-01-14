1d29722bd3075602549fa7234c2216730d7c3464
Andres Icedo <andresicedo24@gmail.com>
2021-01-14 11:09:00 -0600

    madlib function file

---
+++ heroes.py
@@ -0,0 +1,4 @@
+heroes = ['Iron Man', 'Captain America', 'The Hulk']
+
+for i in range(len(heroes)):
+    print(heroes[i])
---
+++ madlib_function.py
@@ -0,0 +1,20 @@
+'''
+1. Madlib function
+Write a function that accepts two arguments: a name and a subject.
+
+The function should return a String with the name and 
+subject interpolated in.
+
+For example:
+
+madlib("Jenn", "science")
+# "Jenn's favorite subject is science."
+
+madlib("Jeff", "history")
+# "Jeff's favorite subject is history."
+Provide default arguments in case one or both are omitted.
+'''
+def madlib(name, subject):
+    return f"{name}'s favorite subject is {subject}."
+fav = madlib("Jeff", "lunch")
+print(fav)
